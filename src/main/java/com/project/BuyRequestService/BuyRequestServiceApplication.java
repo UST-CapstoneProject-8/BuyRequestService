eb4a82d23c8330c693eff51a1667c1c6763cb0b6
maisasugandh <118497024+maisasugandh@users.noreply.github.com>
2025-01-24 22:58:06 +0530

    Created and completed with endpoints for buyrequest

---
+++ src/main/java/com/project/BuyRequestService/BuyRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.BuyRequestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyRequestServiceApplication.class, args);
+	}
+
+}
